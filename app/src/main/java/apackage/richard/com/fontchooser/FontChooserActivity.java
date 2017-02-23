e220dc1598f2ad7daf72005a343b34504d51df11
redferret <RDepsil6!!201+6>
2017-02-23 16:34:37 -0700

    First Commit, FontChooser code

---
+++ app/src/main/java/apackage/richard/com/fontchooser/FontChooserActivity.java
@@ -0,0 +1,13 @@
+package apackage.richard.com.fontchooser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FontChooserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_font_chooser);
+    }
+}
