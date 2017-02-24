7993c7a6652e2fe10e9130633f3077f924dcbbea
redferret <RDepsil6!!201+6>
2017-02-23 17:01:05 -0700

    Added more widgets for selecting font styles and the family

--- app/src/main/java/apackage/richard/com/fontchooser/FontChooserActivity.java
+++ app/src/main/java/apackage/richard/com/fontchooser/FontChooserActivity.java
@@ -2,6 +2,8 @@ package apackage.richard.com.fontchooser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class FontChooserActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class FontChooserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_font_chooser);
+        setupFontSpinners();
     }
+
+    public void setupFontSpinners(){
+        Spinner spinner = (Spinner) findViewById(R.id.fontStyles);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.fontStyles, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner = (Spinner) findViewById(R.id.fontFamily);
+        adapter = ArrayAdapter.createFromResource(this,
+                R.array.fontFamily, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+    }
+
 }
